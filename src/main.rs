d5b06678435ca25c58f787a40eb662d0bbc1e1f6
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2021-03-23 04:54:54 +0000

    Add runner for user actions

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,38 @@ use binance::account::Account;
 use binance::model::Order;
 use binance::websockets::*;
 
+use atomic_float::AtomicF64;
+use core::sync::atomic::Ordering::Relaxed;
+
 fn main() {
 
     let account: Account = load_account();
 
     let symbol = "DENTUSDT";
-    let margin: f32 = 0.1;
+    let margin: f32 = 0.01;
+
+    let symbol_c = symbol.clone();
+    let margin_c = margin.clone();
+    let acc_c = account.clone();
+
+    let price_monitor_thread = std::thread::spawn(move || {
+        price_monitor(account, symbol, margin);
+    });
+
+    let user_monitor_thread = std::thread::spawn(move || {
+        user_monitor(acc_c, symbol_c, margin_c);
+    });
+
+    let _ = price_monitor_thread.join();
+    let _ = user_monitor_thread.join();
+
+}
+
+fn price_monitor(account: Account, symbol: &str, margin: f32) {
 
     let order_found = AtomicBool::new(false);
+
+    static HIGHEST_PRICE: AtomicF64 = AtomicF64::new(0.0);
 
     let keep_running = AtomicBool::new(true); // Used to control the event loop
     let agg_trade: String = format!("!ticker@arr"); // All Symbols
@@ -29,17 +53,26 @@ fn main() {
                         // let symbol_average: f32 = tick_event.average_price.parse().unwrap();
                         let symbol_close: f32 = tick_event.current_close.parse().unwrap();
 
-                        let margin_price: f64 = f64::from(symbol_close * margin);
-                        let recommended_price_stop: f64 = f64::from(symbol_close) - margin_price;
-
-                        println!("Close: {}, Recommended price: {}", symbol_close, recommended_price_stop);
+                        println!("Latest close price: {}", symbol_close);
                         
                         match get_open_order(&account, symbol) {
                             Some(order) => {
+
                                 order_found.store(true, Ordering::Relaxed);
+        
+                                let margin_price: f64 = f64::from(symbol_close * margin);
+                                let recommended_price_stop: f64 = f64::from(symbol_close) - margin_price;
+
+                                let highest_recommended_price_stop: f64 = HIGHEST_PRICE.load(Ordering::Relaxed);
+                                let max_recommended_price_stop: f64 = highest_recommended_price_stop.max(recommended_price_stop);
+                                HIGHEST_PRICE.store(max_recommended_price_stop, Ordering::Relaxed);
+        
+                                println!("Close: {}, Recommended price: {}", symbol_close, max_recommended_price_stop);
+
                                 let diff: f64 = f64::from(symbol_close) - order.price;
+                                println!("diff {}, {}", diff, margin_price);
                                 if diff > margin_price {
-                                    update_stop_loss(&account, order, recommended_price_stop);
+                                    update_stop_loss(&account, order, max_recommended_price_stop);
                                 } else {
                                     println!("Keep stop loss");
                                 }
@@ -76,6 +109,47 @@ fn main() {
 
 }
 
+use binance::userstream::*;
+
+fn user_monitor(account: Account, symbol: &str, margin: f32) {
+
+    let user_stream: UserStream = user_stream();
+    let keep_running = AtomicBool::new(true);
+
+    if let Ok(answer) = user_stream.start() {
+
+        let listen_key = answer.listen_key;
+    
+        let mut web_socket: WebSockets = WebSockets::new(|event: WebsocketEvent| {
+            match event {
+            WebsocketEvent::AccountUpdate(account_update) => {
+                for balance in &account_update.balance {
+                println!("Asset: {}, free: {}, locked: {}", balance.asset, balance.free, balance.locked);
+                }
+            },
+            WebsocketEvent::OrderTrade(trade) => {
+                println!("Symbol: {}, Side: {}, Price: {}, Execution Type: {}", trade.symbol, trade.side, trade.price, trade.execution_type);
+            },
+            _ => (),
+            };
+            Ok(())
+        });
+    
+        web_socket.connect(&listen_key).unwrap(); // check error
+            if let Err(e) = web_socket.event_loop(&keep_running) {
+            match e {
+                err => {
+                    println!("Error: {:?}", err);
+                }
+            }
+        }
+
+        return
+    }
+
+    println!("Not able to start an User Stream (Check your API_KEY)");
+}
+
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Config {
@@ -84,6 +158,16 @@ struct Config {
 }
 
 fn load_account() -> Account {
+    let mut file = File::open("env.toml").unwrap();
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).unwrap();
+    let config: Config = toml::from_str(&buffer).unwrap();
+    let api_key = Some(config.api_key.into());
+    let secret_key = Some(config.secret_key.into());
+    Binance::new(api_key, secret_key)
+}
+
+fn user_stream() -> UserStream {
     let mut file = File::open("env.toml").unwrap();
     let mut buffer = String::new();
     file.read_to_string(&mut buffer).unwrap();
